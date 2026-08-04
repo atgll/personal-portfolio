542e48e6941aa39c10ad384717aa74b9b7466485
atgll <angelt.guillo@gmail.com>
2026-08-04 17:57:45 +0200

    elimnado proyecto portfolio duplicado para pruebas

--- src/data/projects.data.ts
+++ src/data/projects.data.ts
@@ -13,15 +13,5 @@ export const projects: ProjectI[] = [
         images: [portfolioHome],
         link: 'https://www.angeltorresweb.com/',
         techStack: [nodeJs, css, js, git, mongodb]
-    },
-    {
-        id: 2,
-        title: 'Portfolio ATG',
-        description: 'Portfolio personal, centrado en una estética clara y limpia, una navegación fluida y un despliegue inteligente usando las plataformas y servicios más adecuados a cada necesidad',
-        start: '10-07-2026',
-        end: '',
-        images: [portfolioHome],
-        link: 'https://www.angeltorresweb.com/',
-        techStack: [nodeJs, css, js, git, mongodb]
     }
 ]
