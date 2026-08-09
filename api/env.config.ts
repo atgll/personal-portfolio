42f1d06075b1d2ea43dfa90d803b91e9aecc78e6
atgll <angelt.guillo@gmail.com>
2026-08-09 14:58:00 +0200

    Start mail-service backend API

---
+++ api/env.config.ts
@@ -0,0 +1,57 @@
+import dotenv from "dotenv";
+import {fileURLToPath} from "node:url";
+import path from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({
+    path: path.resolve(__dirname, '../', '.env')
+});
+
+export interface EnvConfigInterface {
+    host: string;
+    port: number;
+    user: string;
+    appPass: string;
+
+    from: string;
+    to: string;
+}
+
+export function getEnvVariables(key: string, defaultValue?: string): string {
+    const value = process.env[key]
+
+    if(!value && !defaultValue) {
+        throw new Error(
+            `No existen las variables de entorno requeridas: ${key}\n`
+        );
+    }
+    return value || defaultValue!;
+}
+
+export function getEnvNumVariables(key: string, defaultValue?: number): number {
+    const value = process.env[key]
+
+    if(!value && defaultValue === undefined) {
+        throw new Error(
+            `No existen las variables de entorno requeridas: ${key}\n`
+        );
+    }
+
+    const  numValue: number | undefined = value ? parseInt(value, 10) : defaultValue!;
+
+    if(isNaN(numValue)) {
+        throw new Error(`${key} should be a number`);
+    }
+
+    return numValue;
+}
+
+export const env: EnvConfigInterface = {
+    host: getEnvVariables('GMAIL_HOST'),
+    port: getEnvNumVariables('GMAIL_PORT'),
+    user: getEnvVariables('GMAIL_USER'),
+    appPass: getEnvVariables('GMAIL_APP_PASSWORD'),
+    from: getEnvVariables('MAIL_FROM'),
+    to: getEnvVariables('MAIL_TO')
+}
