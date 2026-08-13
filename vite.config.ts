399f5555f6214fd048bcf71d72919a460d2ff3de
atgll <angelt.guillo@gmail.com>
2026-08-13 17:49:56 +0200

    Modified gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import 'dotenv/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.SITE_KEY': JSON.stringify(process.env.SITE_KEY ?? ''),
+  },
 })
