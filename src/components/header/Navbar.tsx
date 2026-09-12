623a058765471eca72d3cdc4ac57ef18a90bdd76
atgll <angelt.guillo@gmail.com>
2026-09-12 22:50:47 +0200

    Cambios n los nombres de navbar usando section de sections.data

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,5 +1,7 @@
 import {NavLink} from "react-router";
 import type {ReactElement} from "react";
+import {sections} from "../../data";
+import type {SiteSectionI} from "../../interfaces";
 
 const Navbar = ({variant}: {variant: 'home' | 'page'}): ReactElement => {
 
@@ -9,9 +11,15 @@ const Navbar = ({variant}: {variant: 'home' | 'page'}): ReactElement => {
         <nav className={wrapperClass}>
             <ul className='d-flex gap-1 justify-start align-center inter-text fs-l'>
                 <li><NavLink to='/' viewTransition={true} className='link' end>Home</NavLink></li>
-                <li><NavLink to='/about' viewTransition={true} className='link' end>About</NavLink></li>
-                <li><NavLink to='/projects' viewTransition={true} className='link' end>Projects</NavLink></li>
-                <li><NavLink to='/contact' viewTransition={true} className='link' end>Contact</NavLink></li>
+                {
+                    sections.map((section: SiteSectionI)=> (
+                        <li><NavLink to={section.path} viewTransition={true} className='link' end>{section.name}</NavLink></li>
+                    ) )
+                }
+                {/*<li><NavLink to='/' viewTransition={true} className='link' end>Home</NavLink></li>
+                <li><NavLink to='/sobreMi' viewTransition={true} className='link' end>About</NavLink></li>
+                <li><NavLink to='/proyectos' viewTransition={true} className='link' end>Projects</NavLink></li>
+                <li><NavLink to='/contact' viewTransition={true} className='link' end>Contact</NavLink></li>*/}
             </ul>
         </nav>
     )
